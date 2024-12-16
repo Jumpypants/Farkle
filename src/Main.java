9de82f6c83fd8e17df7d5a1e4016fe69c5a3568a
Jumpypants <jumpypants10@gmail.com>
2024-12-16 11:06:30 -0800

    ieurafncawilcunlowBIU

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
@@ -10,18 +11,7 @@ public class Main {
 			scores.add(0);
 		}
 
-		for (int i = 0; i < numRounds; i++) {
-			for (int j = 0; j < numPlayers; j++) {
-				System.out.println("--PLAYER " + (j + 1) + " TURN");
-				Turn turn = new Turn();
-				scores.set(j, scores.get(j) + turn.score);
-
-				System.out.println("--Scores:");
-				for (int k = 0; k < numPlayers; k++) {
-					System.out.println("Player " + (k + 1) + ": " + scores.get(k));
-				}
-			}
-		}
+		play(numPlayers, numRounds, scores);
 
 		int highIndex = 0;
 		int highScore = 0;
@@ -34,5 +24,20 @@ public class Main {
 
 		System.out.println();
 		System.out.println("PLAYER " + (highIndex + 1) + " WINS!");
+	}
+
+	private static void play(int numPlayers, int numRounds, List<Integer> scores) {
+		for (int i = 0; i < numRounds; i++) {
+			for (int j = 0; j < numPlayers; j++) {
+				System.out.println("--PLAYER " + (j + 1) + " TURN");
+				Turn turn = new Turn();
+				scores.set(j, scores.get(j) + turn.score);
+
+				System.out.println("--Scores:");
+				for (int k = 0; k < numPlayers; k++) {
+					System.out.println("Player " + (k + 1) + ": " + scores.get(k));
+				}
+			}
+		}
 	}
 }
